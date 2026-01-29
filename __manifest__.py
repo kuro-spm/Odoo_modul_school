2c6fa48c33601c9d36981633c8413d415df67c0f
kuro-spm <sarapratsm@gmail.com>
2026-01-29 18:44:21 +0100

    primer commit

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'School',
+    'version': '2.0',
+    'category': 'Education',
+    'summary': 'School Management',
+    'description': """
+          Module prepared by department 'Informàtica i comunicacions'
+          of Institute Milà i Fontanals in Igualada (Barcelona-Spain)
+          for learning in development and adaptation of modules of Odoo ERP.
+
+          It is part of the learning materials for the module
+          'Sistemes de gestió empresarial' in the course
+          'CFS Desenvolupament d''aplicacions multiplataforma'.
+    """,
+    'author': 'Group DAM2 - Course 2025-2026',
+    'website': 'http://www.infomila.info',
+    'depends': ['base'],
+    'data': ['views/school_views.xml'],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ models/school.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from odoo import models,fields
+
+class SchoolCourse(models.Model):
+    _name = 'school.course'
+    _description = 'Course Management'
+
+    name = fields.Char('Name', size=60, required=True)
+    hours = fields.Integer('Hours', required=True)
+    active = fields.Boolean('Active', default=True)
+    manager_id = fields.Many2one('school.teacher', 'Manager') #No és required perquè és 0..1
+
+
+class SchoolSubject(models.Model):
+    _name = 'school.subject'
+    _description = 'Subject Management'
+
+    name = fields.Char('Name', size=60, required=True)
+    hours = fields.Integer('Hours', required=True)
+    active = fields.Boolean('Active', default=True)
+
+class SchoolTeacher(models.Model):
+    _name = 'school.teacher'
+    _description = 'Teacher Management'
+
+    first_name = fields.Char('First Name', size=30, required=True)
+    last_name = fields.Char('Last Name', size=40, required=True)
+    birthdate = fields.Date('Birthdate', required=True)
+    tin = fields.Char('Tax ID', size=14)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')],'Gender')
+    salary = fields.Integer('Salary')
+    email = fields.Char('eMail', size=60, required=True)
+    phone = fields.Char('Phone')

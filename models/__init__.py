64ab2ce075f52d44574068c16bb5ed5843d8ecd6
kuro-spm <sarapratsm@gmail.com>
2026-04-22 14:59:06 +0200

    models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from . import school
+from . import school_course
+from . import school_subject
+from . import school_teacher
+from . import course_subject
+from . import school_course_edition
+from . import school_thematic
+from . import teaching
+from . import teacher_salary_history

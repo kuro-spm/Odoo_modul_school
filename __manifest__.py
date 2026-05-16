b3e2fd568618866185f1d059663f7a9c84381560
kuro-spm <sarapratsm@gmail.com>
2026-05-16 23:40:51 +0200

    wizards al manifest

--- __manifest__.py
+++ __manifest__.py
@@ -33,6 +33,7 @@
         'reports/report_school_course.xml',
         'reports/report_school_course_edition.xml',
         'reports/report_school_teacher.xml',
+        'wizard/how_many_editions_betweeen_dates.xml',
      
     ],
     'demo': [],
